40d449a802037c0e2cb266956f6232a014f9a570
sammarsz <sammarsz61@gmail.com>
2024-11-02 04:17:53 +0000

    Add files via upload

---
+++ Borderless verification/faq.js
@@ -0,0 +1,8 @@
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(question => {
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling;
+    answer.classList.toggle('show');
+  });
+});
